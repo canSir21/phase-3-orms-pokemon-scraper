a9fc358c7d748d5c24d7e4d6e5ac7f8b3ea65724
canSir21 <alex.mwaura@moringaschool.com>
2022-09-20 18:37:18 +0300

    commit tree 4f143723e53fc2b237e0979f31e2396f5e072fe1

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+    
+    def initialize attributes
+        attributes.each {|key, value|
+            self.class.attr_accessor(key)
+            self.send("#{key}=",value)
+        }
+    end
+    def self.save name, type, db 
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+    def self.find id_num, db
+        pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+        Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+        
+    end
 end
